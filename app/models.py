75f37f58089b835096d0b9ba3b5495c18771363f
Nicholas Joiles <kemanijoiles@gmail.com>
2024-04-04 23:30:50 -0500

    created form class

--- app/models.py
+++ app/models.py
@@ -1,1 +1,19 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+
+class MovieForm(db.Model):
+    # You can use this to change the table name. The default convention is to use
+    # the class name. In this case a class name of UserProfile would create a
+    # user_profile (singular) table, but if we specify __tablename__ we can change it
+    # to `user_profiles` (plural) or some other name.
+    __tablename__ = 'properties'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),  unique=True)
+    description = db.Column(db.String(255))
+    num_rooms = db.Column(db.Integer)
+    num_brooms = db.Column(db.Integer)
+    price = db.Column(db.String(255))
+    ptype =  db.Column(db.String(255))
+    location = db.Column(db.String(255))
+    pname = db.Column(db.String(255))
